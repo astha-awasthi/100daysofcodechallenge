6a6db7bcfcfb8d12b62113944de1a1eb4d7e363d
ASTHA AWASTHI <networkbusy0510@gmail.com>
2022-10-22 18:06:21 +0530

    Create SolutionCode.java

---
+++ Day 17- Strings-Length of Last word/SolutionCode.java
@@ -0,0 +1,16 @@
+***SOLUTION***
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+        {
+        int i = 0;
+        int flag = 1;
+        s=s.trim();
+
+        for (i= s.lastIndexOf(" ")+1; i<s.length()-1;i++) {
+            flag++;
+        }
+        return flag;
+    }
+}
+}
