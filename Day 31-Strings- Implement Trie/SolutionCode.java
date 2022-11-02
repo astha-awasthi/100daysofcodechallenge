bf13ac97fdf15550293917d32f4c18b824190d9a
ASTHA AWASTHI <networkbusy0510@gmail.com>
2022-11-02 07:45:27 +0530

    Create SolutionCode.java

---
+++ Day 31-Strings- Implement Trie/SolutionCode.java
@@ -0,0 +1,61 @@
+class Trie {
+    
+    class Node {
+        Node [] childs;
+        boolean isEnd;
+        
+        Node(){
+            childs = new Node[26];
+            isEnd = false;
+        }
+    }
+    
+    final private Node root;
+    
+    public Trie() {
+        root = new Node();
+    }
+    
+ 
+    public void insert(String word) {
+        Node curr = root;
+        
+        for(int i = 0;i<word.length();i++){
+            char ch = word.charAt(i);
+            
+            if(curr.childs[ch - 'a'] == null){
+                curr.childs[ch - 'a'] = new Node();
+            }
+            curr = curr.childs[ch - 'a'];
+        }
+        
+        curr.isEnd = true;
+    }
+    
+ 
+    public boolean search(String word) {
+        Node curr = root;
+        
+        for(int i = 0;i<word.length();i++){
+            char ch = word.charAt(i);
+            
+            if(curr.childs[ch - 'a'] == null) return false;
+            curr = curr.childs[ch - 'a'];
+        }
+        return curr.isEnd;
+    }
+    
+  
+    public boolean startsWith(String prefix) {
+          Node curr = root;
+        
+        for(int i = 0;i<prefix.length();i++){
+            char ch = prefix.charAt(i);
+            
+            if(curr.childs[ch - 'a'] == null) return false;
+            curr = curr.childs[ch - 'a'];
+        }
+        
+        return true;
+    }
+}
