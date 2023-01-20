2454ba35bd1de4c9de49e47b5f294b27504aedf7
ASTHA AWASTHI <networkbusy0510@gmail.com>
2023-01-20 07:33:03 +0530

    Create SolutionCode.java

---
+++ Day 76- Queue - Time needed to buy Tickets/SolutionCode.java
@@ -0,0 +1,13 @@
+
+class Solution {
+	 public String makeGood(String s) {
+        for (int i = 0; i < s.length() - 1; i++) {
+           
+            if (Math.abs(s.charAt(i) - s.charAt(i+1)) == 32) {  
+               
+                return makeGood(s.substring(0, i) + s.substring(i+2));
+            }
+        }     
+        return s;
+    }
+}
